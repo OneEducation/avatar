765637b77e56ee2f03ba1efc850777ad8f13b3d4
dongseok paeng <dongseok0@dongseok-ui-MacBook-Pro.local>
2014-12-17 15:45:38 +1100

    Apply immersive full screen mode

--- app/src/main/java/org/olpc/avatargen/MainActivity.java
+++ app/src/main/java/org/olpc/avatargen/MainActivity.java
@@ -88,10 +88,12 @@ public class MainActivity extends Activity {
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        // making it full screen
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
         // Get the intent that started this activity
         Intent intent = getIntent();
